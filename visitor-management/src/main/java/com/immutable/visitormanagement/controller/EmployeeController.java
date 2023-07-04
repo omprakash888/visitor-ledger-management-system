4e040d39f1e9ca19dcad870457f33563e9849f77
Shalini B <shalini.b@olamnet.com>
2023-07-04 12:20:49 +0530

    employee CRUD

--- visitor-management/src/main/java/com/immutable/visitormanagement/controller/EmployeeController.java
+++ visitor-management/src/main/java/com/immutable/visitormanagement/controller/EmployeeController.java
@@ -1,4 +1,41 @@
 package com.immutable.visitormanagement.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpServerErrorException;
+
+import com.immutable.visitormanagement.dto.EmployeeDto;
+import com.immutable.visitormanagement.dto.VisitorDto;
+import com.immutable.visitormanagement.service.EmployeeService;
+
+import jakarta.validation.Valid;
+
+@RestController
 public class EmployeeController {
+	@Autowired
+	private EmployeeService employeeservices;
+	
+	@PostMapping("/createemployee")
+	public ResponseEntity<String> createEmployee(@Valid @RequestBody EmployeeDto employeeDto){
+		employeeservices.save(employeeDto);
+		return new ResponseEntity<>("Employee Created Successfully",HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/getemployees")
+	public ResponseEntity<List<VisitorDto>> getAllEmployees() {
+        List<EmployeeDto> employeeDtos = this.employeeservices.getAllEmployees();
+        return new ResponseEntity<>(employeeDtos,HttpStatus.OK);
+    }
+	
+	
+
+
 }
--- visitor-management/src/main/java/com/immutable/visitormanagement/service/EmployeeService.java
+++ visitor-management/src/main/java/com/immutable/visitormanagement/service/EmployeeService.java
@@ -1,4 +1,14 @@
 package com.immutable.visitormanagement.service;
 
+import java.util.List;
+
+import com.immutable.visitormanagement.dto.EmployeeDto;
+
+
 public interface EmployeeService {
+	
+	public void save(EmployeeDto employeeDto);
+
+    public List<EmployeeDto> getAllEmployees();
+
 }
